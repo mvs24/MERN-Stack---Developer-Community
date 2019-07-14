5f49d45b27202ef5e42f87167545c8b89ba3ebf6
mvs24 <40918003+mvs24@users.noreply.github.com>
2019-07-14 18:09:16 +0200

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,9 @@ module.exports= passport => {
     passport.use(new JwtStrategy(opts, (jwt_payload, done)=>{
         // console.log(jwt_payload);
         User.findById(jwt_payload.id).then(user=>{
+            //console.log(user);
             if(user){
+                
                 return done(null, user);
             }
             return done(null, false);
