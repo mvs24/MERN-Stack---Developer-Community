e2c32342ccae45371b74d89cdc86a7a02bcae0d4
Marius <Marius.Vasili@fti.edu.al>
2019-09-04 11:08:45 +0200

    Ready for production

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
 
-const users=require('./routes/api/users');
-const profile=require('./routes/api/profile');
-const posts=require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// 
-
+//
 
 // DB config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 // connect to MongoDB
-mongoose.connect(db, { useNewUrlParser: true }).then(()=>{
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
     console.log("MongoDB connected");
-}).catch(err=>{
+  })
+  .catch(err => {
     console.log("Error: " + err);
-})
+  });
 
 // passport middleware
 app.use(passport.initialize());
 
-
 // passport config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Use routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
-
-
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
-app.listen(port, ()=>{
-    console.log("listening on port " + port);
-})
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
